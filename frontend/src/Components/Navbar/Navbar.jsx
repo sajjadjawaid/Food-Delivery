f7536b81f52d22852349a30645e0826287b605a1
sajjadjawaid <sajjadjawaid@gmail.com>
2024-07-08 00:27:57 +0500

    navbar

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { assets } from '../../assets/assets'
+import './Navbar.css'
+
+const Navbar = () => {
+
+  const [ menu, setMenu] = useState("");
+  return (
+    <div className='navbar'>
+        <img src={assets.logo} alt='' className='logo'/>
+      <ul className="navbar-menu">
+        <li onClick={() =>{
+          setMenu("home")
+          console.log(menu)
+        }} className={menu === "home"? "active": ""}>Home</li>
+        <li 
+        onClick={() =>{
+          setMenu("menu")
+          console.log(menu)
+        }}
+        className={menu === "menu"? "active": ""}>Menu</li>
+        <li 
+        onClick={() =>{
+          setMenu("mobile-app")
+        }}
+        className={menu === "mobile-app"? "active": ""}>Mobile-App</li>
+        <li 
+        onClick={() =>{
+          setMenu("contact-us")
+        }}
+        className={menu === "contact-us"? "active": ""}>Contact Us</li>
+        </ul>  
+        <div className='navbar-right'>
+           <img src={assets.search_icon} alt=''/>
+           <div className='navbar-search-icon'>
+            <img src={assets.basket_icon} alt=''/>
+            <div className="dot"></div>
+           </div>
+           <button>
+            SignIn
+           </button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
